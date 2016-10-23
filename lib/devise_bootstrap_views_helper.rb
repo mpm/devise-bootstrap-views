dc350a1c513b43e9afc7642d59c46162fdb48ed5
Malte Münchert <malte.muenchert@gmx.net>
2016-10-23 22:33:28 +0200

    Do not lowercase model name when generating the error message summary.

--- lib/devise_bootstrap_views_helper.rb
+++ lib/devise_bootstrap_views_helper.rb
@@ -7,7 +7,7 @@ module DeviseBootstrapViewsHelper
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     sentence = I18n.t('errors.messages.not_saved',
       count: resource.errors.count,
-      resource: resource.class.model_name.human.downcase)
+      resource: resource.class.model_name.human)
 
     html = <<-HTML
     <div class="alert alert-danger alert-block devise-bs">
